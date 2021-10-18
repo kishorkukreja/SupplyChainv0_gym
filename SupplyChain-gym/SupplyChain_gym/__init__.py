8dc5f9ebdb2ce7e8bb81cb860dfb7a7a3650f057
Kishor Kukreja <kishorkukreja76@gmail.com>
2021-10-18 20:00:37 +0100

    Update __init__.py

--- SupplyChain-gym/SupplyChain_gym/__init__.py
+++ SupplyChain-gym/SupplyChain_gym/__init__.py
@@ -4,3 +4,8 @@ register(
     id="SupplyChainEnv-v0",
     entry_point="SupplyChain_gym.envs:InventoryEnv",
 )
+
+register(
+    id="NetworkManagement-v0",
+    entry_point="SupplyChain_gym.envs:NetInvMgmtBacklogEnv",
+)
