bc7738f90a0963f9dfc02c1c38e0635d5b685286
Kishor Kukreja <kishorkukreja76@gmail.com>
2021-10-18 20:07:40 +0100

    Update setup.py

--- SupplyChain-gym/setup.py
+++ SupplyChain-gym/setup.py
@@ -1,6 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import sys
+import os
 
 setup(name="SupplyChain_gym",
       version="1.0.0",
-      install_requires=["gym"]
+      install_requires=[
+		'gym>=0.15.0',
+		'numpy>=1.16.1',
+		'scipy>=1.0',
+		'matplotlib>=3.1',
+		'networkx>=2.3'],
+      python_requires='>=3.5',
+      packages=find_packages(),
+      zip_safe=False,
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+      ]
 )  
